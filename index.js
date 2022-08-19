57c34a62db4cc4bc32d476c4b0352a7ff8a5b530
ShaunM <seansama__@pop-os.localdomain>
2022-08-19 11:52:53 +0300

    Done with assignment

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// code your solution here
+const saturdayFun = (param)=> {
+    if (param=== `bathe my dog`){
+        return `This Saturday, I want to ${param}!`
+    }
+    return `This Saturday, I want to roller-skate!`
+}
+
+const mondayWork =(param)=>{
+    if (param=== `work from home`){
+        return `This Monday, I will ${param}.`
+    }
+    return `This Monday, I will go to the office.`
+}
+
+function wrapAdjective (closers) {
+    return function(param = `a hard worker`) {
+        return `You are ${closers}${param}${closers}!`
+    }
+}
